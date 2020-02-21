feecc6b418412fd0c3bbafb6249a24ae6ff6bbf6
DanielMoran98 <33605732+DanielMoran98@users.noreply.github.com>
2020-02-21 02:33:00 +0000

    Marker state testing

--- client/src/components/Marker.js
+++ client/src/components/Marker.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import M from 'materialize-css';
 import './SideNav.css'
 
+
 class Marker extends Component {
   constructor(props){
       super(props);
@@ -10,11 +11,14 @@ class Marker extends Component {
           color: props.color
       };
   }
-    
+  updateColor(){
+    this.setState({color:"green"})
+  }
+
   render() {
     return (
         <div className="markerContainer">
-            <img src={"/images/markers/pin-"+this.props.color+".png"} style={{backgroundcolor: this.props.color}} height="24px"/>
+            <a href="#" onClick={() => this.setState({color: "green"})}><img src={"/images/markers/pin-"+this.state.color+".png"} style={{backgroundcolor: this.props.color}} height="24px"/></a>
         </div>  
     )
   }
